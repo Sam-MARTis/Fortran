363d4a103504e5b26588c133bed27c81b9288fad
Samanth Martis <samanthmartis@gmail.com>
2024-05-27 11:24:01 +0530

    Implied Do loops for array constructors

--- 02_data-structures/02_arrays.f95
+++ 02_data-structures/02_arrays.f95
@@ -1,15 +1,21 @@
 program arrays_2
 implicit none
 integer, dimension(10):: arr1
+! integer :: i 
+! integer :: j
+integer :: arr(4,4)
 integer:: i, j
 real:: arr2(10, 10, 10)
 integer:: arr3(0: 5) ! Inclusive of 0 and 5
-integer:: arr4(5, 5)
+! integer:: arr(5, 5)
 
 arr3(:) = 1
 print*, arr3
-arr4 = [(i, i=1,4), :]
-print*, arr4
+! arr = reshape([(i, i = 0, 4, j=0, 4)], [5,5])
+! integer :: arr(4,4)
+
+arr = reshape([((i*j, i=1,4), j=1,4)], shape(arr))
+print*, arr
 
 
 end program arrays_2
